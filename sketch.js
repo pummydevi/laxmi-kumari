9d108b10925b53fbaeb73f15b9533f760860ca4b
pummydevi <89304803+pummydevi@users.noreply.github.com>
2021-09-18 18:34:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,59 +1,150 @@
-var garden,rabbit;
-var gardenImg,rabbitImg;
-var leafImg;
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
+
+var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
+var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
 
 function preload(){
-  gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");
+  
+  knifeImage = loadImage("knife.png");
+  monsterImage = loadAnimation("alien1.png","alien2.png")
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  gameOverImage = loadImage("gameover.png")
+
+  //load sound here
 }
 
-function setup(){
-  
-  createCanvas(400,400);
-  
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
 
-//creating boy running
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+
+function setup() {
+  createCanvas(600, 600);
+  
+  //creating sword
+   knife=createSprite(40,200,20,20);
+   knife.addImage(knifeImage);
+   knife.scale=0.7
+  
+  //set collider for sword
+  knife.setCollider("rectangle",0,0,40,40);
+
+  // Score variables and Groups
+  score=0;
+  fruitGroup=createGroup();
+  monsterGroup=createGroup();
+  
 }
-
 
 function draw() {
-  background(0);
+  background("lightblue");
   
-  edges= createEdgeSprites();
-  rabbit.collide(edges);
-  rabbit.x=World.mouseX;
-
-
-
+  if(gameState===PLAY){
+    
+    //Call fruits and Monster function
+    fruits();
+    Monster();
+    
+    // Move sword with mouse
+    knife.y=World.mouseY;
+    knife.x=World.mouseX;
+  
+    // Increase score if sword touching fruit
+    if(fruitGroup.isTouching(knife)){
+      score=score+2;
+      fruitGroup.destroyEach();
+    }
+    else
+    {
+      // Go to end state if sword touching enemy
+      if(monsterGroup.isTouching(knife)){
+        gameState=END;
+        
+        //add gameover sound here
+        
+        if(fruitGroup.isTouching(knife)){
+          fruitGroup.destroyEach();
+          knifeSwooshSound.play();
+        }
+      
+        
+        monsterGroup.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        monsterGroup.setVelocityXEach(0);
+        
+        // Change the animation of sword to gameover and reset its position
+        knife.addImage(gameOverImage);
+        knife.scale=2;
+        knife.x=300;
+        knife.y=300;
+      }
+    }
+  }
+  
   drawSprites();
-}
-var select_sprites = Math.round(random(1,3));
-if (frameCount % 80 == 0) {
-if (select_sprites  == 1) 
-{ createApples(); }
-else if  (select_sprites == 2) {         
-else {  createApples(); } }
-
+  //Display score
+  textSize(25);
+  text("Score : "+ score,250,50);
 }
 
-function createApples(){
-  apple=createSprite(random(50,350),40,10,10);
-  apple.addImage(apple.png);
-  apple.scale=0.1;
-  apple.velocityY=5;
-  apple.lifetime=150;
+
+function Monster(){
+  if(World.frameCount%200===0){
+    monster=createSprite(400,200,20,20);
+    monster.addAnimation("moving", monsterImage);
+    monster.y=Math.round(random(100,550));
+    //update below give line of code for increase monsterGroup speed by 10
+    monster.velocityX = -8;
+    monster.setLifetime=50;
+    monster.velocityX=-(8+(score/10));
+    monsterGroup.add(monster);
+  }
 }
 
-function createLeaves(){
-  leaf =createSprite(randon(50,350),40,10,10);
-  leaf.addImage(leaf.png);
-  leaf.scale=0.1;
-  leaf.velocityY=5;
-  leaf.lifetime=150;
+function fruits(){
+  if(World.frameCount%80===0){
+    position = Math.round(random(1,2));
+    fruit=createSprite(400,200,20,20);
+    
+     //using random variable change the position of fruit, to make it more challenging
+    
+    if(position==1)
+    {
+    fruit.x=600;
+    //update below give line of code for increase fruitGroup speed by 4
+    fruit.velocityX=-7
+    }
+    else
+    {
+      if(position==2){
+      fruit.x=0;
+      
+     //update below give line of code for increase fruitGroup speed by 4
+      fruit.velocityX= 7;
+      }
+    }
+    
+    fruit.scale=0.2;
+     //fruit.debug=true;
+     r=Math.round(random(1,4));
+    if (r == 1) {
+      fruit.addImage(fruit1);
+    } else if (r == 2) {
+      fruit.addImage(fruit2);
+    } else if (r == 3) {
+      fruit.addImage(fruit3);
+    } else {
+      fruit.addImage(fruit4);
+    }
+    
+    fruit.y=Math.round(random(50,550));
+   
+    
+    fruit.setLifetime=100;
+    
+    fruitGroup.add(fruit);
+  }
 }
